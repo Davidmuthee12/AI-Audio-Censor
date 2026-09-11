3dbee526b48ad05e500a783dc75b2090c71f3a63
unknown <mutheedavid12@gmail.com>
2026-09-11 23:37:43 +0300

    feat: refactor and cleanup stuff

--- app/main.py
+++ app/main.py
@@ -1,68 +1,35 @@
 from contextlib import asynccontextmanager
 
-from fastapi import Depends, FastAPI, UploadFile
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from scalar_fastapi import get_scalar_api_reference
-from sqlmodel import Session
 
-from app.audio_censor import AudioCensor
-from app.database.models import Audio
-from app.database.session import get_session, init_db
-
-UPLOADS_DIR = "uploads"
+from app.api.router import router
+from app.database.session import init_db
+from app.utils import UPLOADS_DIR
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     init_db()
-    print("🟢 Starting up...")
     yield
-    print("🔴 ...shutting down!")
 
 
+# Main FastAPI instance
 app = FastAPI(docs_url=None, lifespan=lifespan)
+
+# Serve uploaded files
 app.mount(
     "/uploads",
     StaticFiles(directory=UPLOADS_DIR),
     name="uploads",
 )
 
-ac = AudioCensor()
+# Include API routes
+app.include_router(router)
 
 
-def save_file(upload_file: UploadFile, path: str):
-    with open(path, "wb") as buffer:
-        buffer.write(upload_file.file.read())
-
-
-@app.post("/censor")
-def censor_audio(audio_file: UploadFile, session: Session = Depends(get_session)):
-    file_path = f"{UPLOADS_DIR}/{audio_file.filename}"
-    output_path = f"{UPLOADS_DIR}/censored_{audio_file.filename}"
-
-    audio = Audio(
-        file_path=file_path,
-        censored_file_path=output_path,
-    )
-    session.add(audio)
-    session.commit()
-    session.refresh(audio)
-
-    save_file(audio_file, file_path)
-
-    words = ac.transcribe_audio(file_path)
-    ac.mute_audio(
-        input_path=file_path,
-        output_path=output_path,
-        word_segments=ac.detect_profanity(words),
-    )
-
-    return {
-        "id": audio.id,
-        "url": f"/uploads/censored_{audio_file.filename}",
-    }
-
-
+# Scalar docs endpoint
 @app.get("/docs", include_in_schema=False)
 def scalar_docs():
     return get_scalar_api_reference(

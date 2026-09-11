4639dd367189e7202eebe266562dfbaf78007043
unknown <mutheedavid12@gmail.com>
2026-09-11 23:36:24 +0300

    feat: transfer core functionality into service layer

---
+++ app/service/audio.py
@@ -0,0 +1,39 @@
+from fastapi import UploadFile
+from sqlmodel import Session
+
+from app.audio_censor import AudioCensor
+from app.database.models import Audio
+from app.utils import get_file_path, save_file
+
+
+class AudioService:
+    def __init__(self, session: Session):
+        self.session = session
+        self.ac = AudioCensor()
+
+    def add_audio(self, file: UploadFile) -> Audio:
+        # Get file paths
+        file_path = get_file_path(file.filename)
+        output_path = get_file_path(f"censored_{file.filename}")
+
+        # Add audio record to database
+        audio = Audio(
+            file_path=file_path,
+            censored_file_path=output_path,
+        )
+        self.session.add(audio)
+        self.session.commit()
+        self.session.refresh(audio)
+
+        # Save uploaded file to disk
+        save_file(file)
+
+        # Censor audio
+        words = self.ac.transcribe_audio(file_path)
+        self.ac.mute_audio(
+            input_path=file_path,
+            output_path=output_path,
+            word_segments=self.ac.detect_profanity(words),
+        )
+
+        return audio

77b7e9afe829af7c3b780aa2811e3621dfc57a3f
unknown <mutheedavid12@gmail.com>
2026-09-17 18:04:19 +0300

    feat: setup celery worker

--- app/config.py
+++ app/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     JWT_SECRET: str
     JWT_ALGORITHM: str
 
+    BROKER_URL: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",

fe485941d2dc414549c578d1569ed5fa58cb211d
unknown <mutheedavid12@gmail.com>
2026-09-18 13:27:41 +0300

    refactor: update audio table

--- app/database/models.py
+++ app/database/models.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from enum import Enum
 from uuid import UUID, uuid4
 
-from sqlmodel import Field, Relationship, SQLModel
+from sqlalchemy.dialects import postgresql
+from sqlmodel import Column, Field, Relationship, SQLModel
 
 
 class AudioStatus(Enum):
@@ -26,17 +27,56 @@ class User(SQLModel, table=True):
         sa_relationship_kwargs={"lazy": "selectin"},
     )
 
+    sound_effects: list["SoundEffect"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+
+
+class SoundEffect(SQLModel, table=True):
+    __tablename__ = "sound_effect"
+
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.now)
+
+    name: str
+    file_path: str
+
+    user_id: UUID = Field(foreign_key="user.id")
+    user: User = Relationship(
+        back_populates="sound_effects",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+
 
 class Audio(SQLModel, table=True):
     __tablename__ = "audio"
 
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"onupdate": datetime.now},
+    )
 
+    name: str
     file_path: str
     censored_file_path: str | None = Field(default=None)
 
     status: AudioStatus = Field(default=AudioStatus.pending)
+
+    transcription: list[dict] | None = Field(
+        default=None, sa_column=Column(postgresql.JSONB)
+    )
+
+    use_beep: bool = Field(default=False)
+    user_list: list[str] | None = Field(
+        default=None, sa_column=Column(postgresql.ARRAY(postgresql.TEXT))
+    )
+    sound_effect_id: UUID | None = Field(default=None, foreign_key="sound_effect.id")
+    sound_effect: SoundEffect = Relationship(
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
     user_id: UUID = Field(foreign_key="user.id")
     user: User = Relationship(

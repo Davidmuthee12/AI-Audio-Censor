6fe5a2d4d949f953e668ecfa1f39a6868bff60bb
unknown <mutheedavid12@gmail.com>
2026-09-21 16:28:02 +0300

    feat: setup R2 config variables

--- app/config.py
+++ app/config.py
@@ -9,6 +9,11 @@ class Settings(BaseSettings):
 
     BROKER_URL: str
 
+    R2_ACCESS_KEY_ID: str
+    R2_SECRET_ACCESS_KEY: str
+    R2_ENDPOINT_URL: str
+    R2_BUCKET_NAME: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
